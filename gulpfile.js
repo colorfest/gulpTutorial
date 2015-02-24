87fbd48e6f0ce240b00c281f54b426e16f0bffcf
K.C. Hunter <kchunter@connectionseducation.com>
2015-02-23 21:58:39 -0500

    Gulp video 2

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,27 @@ var gulp 			= require('gulp');
 var concat 			= require('gulp-concat');
 var rename 			= require('gulp-rename');
 var uglify 			= require('gulp-uglify');
+var runSequence 	= require('run-sequence');
 
 /* tasks */
-gulp.task('default', function ()
+gulp.task('depsjs', function ()
 {
 	return gulp.src(['bower_components/modernizr/modernizr.js',
 		'bower_components/jquery/dist/jquery.js',
 		'bower_components/bootstrap/dist/js/bootstrap.js'])
-		.pipe(concat('gulptutorial.js'))
-		.pipe(gulp.dest('src'))
-		.pipe(rename('gulptutorial.min.js'))
+		.pipe(concat('deps.js'))
+		.pipe(gulp.dest('src'));
+});
+
+gulp.task('depsDist', function ()
+{
+	return gulp.src(['src/deps.js'])
+		.pipe(rename('deps.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('bin'));
+});
+
+gulp.task('default', function (callback)
+{
+	runSequence('depsjs', 'depsDist', callback);
 });
